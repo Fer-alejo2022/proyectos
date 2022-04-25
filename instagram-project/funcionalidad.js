904b4adfd16d8c22e6cfd9f0cdb4949e973ba325
Fer-alejo2022 <fer.developer.1998@gmail.com>
2022-04-24 22:18:31 -0400

    updated

--- instagram-project/funcionalidad.js
+++ instagram-project/funcionalidad.js
@@ -1,62 +1,62 @@
-const cor = document.getElementById("correo");
-const nombre = document.getElementById("name");
-const usuarios = document.getElementById("usuario");
-const contrasena = document.getElementById("clave");
-const form = document.getElementById("formulario");
+const email = document.getElementById("email");
+const name = document.getElementById("name");
+const users = document.getElementById("users");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
 
-const expresiones_regulares = {
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+const regular_expressions = {
+	em: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
-function soloLetras(e) {
+function only_letters(e) {
     key = e.keyCode || e.which;
     tecla = String.fromCharCode(key).toLowerCase();
-    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-    especiales = [8, 37, 39, 46];
+    letters = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+    special = [8, 37, 39, 46];
 
-    tecla_especial = false
-    for(var i in especiales) {
-        if(key == especiales[i]) {
-            tecla_especial = true;
+    key_special = false
+    for(var i in special) {
+        if(key == special[i]) {
+            key_special = true;
             break;
         }
     }
 
-    if(letras.indexOf(tecla) == -1 && !tecla_especial)
+    if(letters.indexOf(tecla) == -1 && !key_special)
         event.returnValue= false;
 }
 
 form.addEventListener("submit", (e) => {
-    const alert = document.getElementById("alerta");
+    const alert = document.getElementById("alert");
     
     e.preventDefault();
-    let peligro = "";
-    let entrar = false;
+    let warning = "";
+    let get = false;
     alert.innerHTML = "";
     
-    if (!expresiones_regulares.correo.test(cor.value)) {
-        peligro += " el correo no es valido <br>";
-        entrar = true;
+    if (!regular_expressions.em.test(email.value)) {
+        warning += "the email is not valid <br>";
+        get = true;
     } 
-     if( nombre.value.length < 2) {
-        peligro += "Complete el Campo Nombre, Nombre muy Corto <br>";
-        entrar = true;
+     if(name.value.length < 2) {
+        warning += "fill in the name field, very short name <br>";
+        get = true;
     }
-    if(usuarios.value.length < 5 ) {
-        peligro += "Complete el campo usuario, usuario muy corto <br>";
-        entrar = true;
+    if(users.value.length < 5 ) {
+        warning += "fill in the users field, very short users <br>";
+        get = true;
 
     } 
-    if(contrasena.value.length < 4) {
-         peligro += "Complete el campo Contraseña, Contraseña muy corta <br>";
-         entrar = true;   
+    if(password.value.length < 4) {
+         warning += "fill in the password field, very short password <br>";
+         get = true;   
     }
 
-    if(entrar) {
-         alert.innerHTML = peligro;
+    if(get) {
+         alert.innerHTML = warning;
     } else {
         form.reset();
-        alert.innerHTML = "Registro Existoso"
+        alert.innerHTML = "Successful registration";
         alert.style.color = "#3f729b";
         alert.style.right = "5px"
     }
